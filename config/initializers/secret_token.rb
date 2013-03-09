8d4c2450cae7bc04587d258478719828b95dad18
Binsztock Remy <remy@rbweb.fr>
2013-03-09 16:06:23 +0100

    redactor with rails3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedactorRails3::Application.config.secret_token = 'd85871b0c63b7ecce9bce75ba8092da8c1e02a665f091e807fa03791eb73a0d193e64e5e7347f902966868be039c40f4aadeabcd6485222cef641fa55e1ba3d4'
